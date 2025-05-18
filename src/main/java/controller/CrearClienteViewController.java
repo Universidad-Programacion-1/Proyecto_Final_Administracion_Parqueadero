d777f8618faed0f08e94f305fa7d69e151f5c480
Chavez-jj <juanj.chavezr@uqvirtual.edu.co>
2025-05-17 23:27:21 -0500

    Interfaz de crear cliente

---
+++ src/main/java/controller/CrearClienteViewController.java
@@ -0,0 +1,74 @@
+package controller;
+
+import org.openjfx.Administracion_Parqueadero.App;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class CrearClienteViewController {
+	
+	App app;
+	
+
+    @FXML
+    private TableView<?> tblListClientes;
+
+    @FXML
+    private TableColumn<?, ?> tbcTelefono;
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private Button dtnAtras;
+
+    @FXML
+    private TextField txtTelefono;
+
+    @FXML
+    private Button btnActualizarCliente;
+
+    @FXML
+    private TableColumn<?, ?> tbcNombre;
+
+    @FXML
+    private Button btbAgregarCliente;
+
+    @FXML
+    private TableColumn<?, ?> tbcCedula;
+
+    @FXML
+    private TextField txtCedula;
+
+    @FXML
+    private TextField txtCorreo;
+
+    @FXML
+    private TableColumn<?, ?> tbcCorreo;
+
+    @FXML
+    void onActualizarCliente( ) {
+
+    }
+
+    @FXML
+    void onAgregarCliente() {
+
+    }
+
+    @FXML
+    void onOpenMenu( ) {
+
+   }
+    
+   public void setApp(App app) {
+	   this.app = app;
+   }
+   
+   
+}
+
+
--- src/main/java/org/openjfx/Administracion_Parqueadero/App.java
+++ src/main/java/org/openjfx/Administracion_Parqueadero/App.java
@@ -4,35 +4,49 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+
+import controller.CrearClienteViewController;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
-
+	
+	private Stage primaryStage;
+    @SuppressWarnings("exports")
     private static Scene scene;
-
+    
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Administracion de Parqueadero");
+		CrearCliente();
     }
+    
+    public void CrearCliente() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(App.class.getResource("CrearCliente.fxml"));
+            AnchorPane rootLayout = (AnchorPane) loader.load();
+            CrearClienteViewController crearCliente = loader.getController();
+            crearCliente.setApp(this);
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
+      
     public static void main(String[] args) {
         launch();
     }
 
+    
 }
